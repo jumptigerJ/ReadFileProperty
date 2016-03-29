3001c3f3474aebdba419f761f0edd1fe4144e76c
jumptigerJ <zhangyanj_light@163.com>
2016-03-29 10:03:36 +0800

    ReadFileProperty

---
+++ src/ReadFileProperty.java
@@ -0,0 +1,31 @@
+import java.io.File;
+
+public class ReadFileProperty {
+
+	public static void main(String[] args) {
+		File file = new File("test.txt");
+		//判断文件是否存在
+		System.out.println("判断文件是否存在"+file.exists());
+		//读取文件名称
+		System.out.println("读取文件名称"+file.getName());
+		//读取文件路径
+		System.out.println("读取文件路径"+file.getPath());
+		//读取文件的绝对路径
+		System.out.println("读取文件的绝对路径"+file.getAbsolutePath());
+		//获取文件父级路径
+		System.out.println("获取文件父级路径"+new File(file.getAbsolutePath()).getParent());
+		//读取文件大小
+		System.out.println("读取文件大小"+ file.length() +"byte");
+		//判断文件是否被隐藏
+		System.out.println("判断文件是否被隐藏"+file.isHidden());
+		//判断文件是否可读
+		System.out.println("判断文件是否可读"+file.canRead());
+		//判断文件是否可写
+		System.out.println("判断文件是否可写"+file.canWrite());
+		//判断文件是否为文件夹
+		System.out.println("判断文件是否为文件夹"+file.isDirectory());
+		
+
+	}
+
+}
